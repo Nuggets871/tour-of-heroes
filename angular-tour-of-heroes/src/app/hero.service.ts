e82111e14a7a40a97a2b2e7f062b6a7be62a5f49
Christopher <christopherbondier@gmail.com>
2024-04-15 17:54:25 +0200

    serveur (pt1)

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -3,24 +3,31 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes'; // URL to the web api
 
   constructor(
     private messageService: MessageService,
+    private http: HttpClient,
   ) { }
 
   // getHeroes(): Hero[]{
   //   return HEROES;
   // }
 
-  getHeroes(): Observable<Hero[]>{
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+  // getHeroes(): Observable<Hero[]>{
+  //   const heroes = of(HEROES);
+  //   this.messageService.add('HeroService: fetched heroes');
+  //   return heroes;
+  // }
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
@@ -28,5 +35,9 @@ export class HeroService {
     // le ! permet de dire que (h => h.id === id) ne sera jamais nul
     this.messageService.add(`HeroService: fetched hero id=${hero.id}`);
     return of(hero);
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`)
   }
 }
