ef58cbed24c9726e25972e60fe57676652cff7d1
Christopher <christopherbondier@gmail.com>
2024-04-15 15:59:57 +0200

    Message component + service

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-
-
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,14 +11,25 @@ import { HEROES } from '../mock-heroes';
   
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  };
-  heroes = HEROES;
-
+  heroes: Hero[] = [];
   selectedHero?:Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService,
+  ) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }  
+
   onSelect(hero: Hero){
     this.selectedHero = hero;
+    this.messageService.add(`HeroComponent: Selected hero id=${hero.id}`)
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
 }
