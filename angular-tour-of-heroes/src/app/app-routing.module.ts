ed1f6ccfad0260582848b269bb2839eacb8adeaa
Christopher <christopherbondier@gmail.com>
2024-04-15 16:31:37 +0200

    ajout du AppRoutingModule

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component'; // <--- Import du composant
 
-const routes: Routes = [];
+// Gère ici les redirections avec le chemin et le composant lié
+const routes: Routes = [
+  {
+    path: 'heroes', component: HeroesComponent // <-- route /heroes qui affiche les Heroes 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
